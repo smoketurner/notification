bf4d77828c486937eb0ab80665d296efa3edbc69
Justin Plock <jplock@smoketurner.com>
2015-07-20 20:26:38 -0400

    Fix up some tests

--- notification-application/src/test/java/com/smoketurner/notification/application/riak/NotificationListResolverTest.java
+++ notification-application/src/test/java/com/smoketurner/notification/application/riak/NotificationListResolverTest.java
@@ -26,6 +26,17 @@ public class NotificationListResolverTest {
     }
 
     @Test
+    public void testSingleSiblingWithDelete() throws Exception {
+        final NotificationListObject list = new NotificationListObject("test");
+        list.addNotification(createNotification(1L));
+        list.deleteNotification(1L);
+        final List<NotificationListObject> siblings = ImmutableList.of(list);
+        final NotificationListObject actual = resolver.resolve(siblings);
+        assertThat(actual.getDeletedIds()).isEmpty();
+        assertThat(actual.getNotifications()).isEmpty();
+    }
+
+    @Test
     public void testMultipleSibling() throws Exception {
         final Notification n1 = createNotification(1L);
         final Notification n2 = createNotification(2L);
@@ -41,14 +52,14 @@ public class NotificationListResolverTest {
         list1.addNotification(n3);
 
         final NotificationListObject list2 = new NotificationListObject("test");
-        list1.addNotification(n2);
-        list1.addNotification(n3);
-        list1.addNotification(n5);
+        list2.addNotification(n2);
+        list2.addNotification(n3);
+        list2.addNotification(n5);
 
         final NotificationListObject list3 = new NotificationListObject("test");
-        list1.addNotification(n6);
-        list1.addNotification(n2);
-        list1.addNotification(n5);
+        list3.addNotification(n6);
+        list3.addNotification(n2);
+        list3.addNotification(n5);
 
         final List<NotificationListObject> siblings = ImmutableList.of(list1,
                 list2, list3);
@@ -66,6 +77,50 @@ public class NotificationListResolverTest {
         assertThat(actual).isEqualTo(expected);
         assertThat(actual.getNotifications()).containsExactly(n6, n5, n4, n3,
                 n2, n1);
+        assertThat(actual.getDeletedIds()).isEmpty();
+    }
+
+    @Test
+    public void testMultipleSiblingWithDeletes() throws Exception {
+        final Notification n1 = createNotification(1L);
+        final Notification n2 = createNotification(2L);
+        final Notification n3 = createNotification(3L);
+        final Notification n4 = createNotification(4L);
+        final Notification n5 = createNotification(5L);
+        final Notification n6 = createNotification(6L);
+
+        final NotificationListObject list1 = new NotificationListObject("test");
+        list1.addNotification(n1);
+        list1.addNotification(n4);
+        list1.addNotification(n2);
+        list1.addNotification(n3);
+        list1.deleteNotification(3L);
+
+        final NotificationListObject list2 = new NotificationListObject("test");
+        list2.addNotification(n2);
+        list2.addNotification(n3);
+        list2.addNotification(n5);
+        list2.deleteNotification(4L);
+
+        final NotificationListObject list3 = new NotificationListObject("test");
+        list3.addNotification(n6);
+        list3.addNotification(n2);
+        list3.addNotification(n5);
+        list3.deleteNotifications(ImmutableList.of(3L, 6L));
+
+        final List<NotificationListObject> siblings = ImmutableList.of(list1,
+                list2, list3);
+
+        final NotificationListObject expected = new NotificationListObject(
+                "test");
+        expected.addNotification(n1);
+        expected.addNotification(n2);
+        expected.addNotification(n5);
+
+        final NotificationListObject actual = resolver.resolve(siblings);
+        assertThat(actual).isEqualTo(expected);
+        assertThat(actual.getNotifications()).containsExactly(n5, n2, n1);
+        assertThat(actual.getDeletedIds()).isEmpty();
     }
 
     @Test
