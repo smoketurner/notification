5a8f44e37cfda7c2d59ba5b924a362c4ecf38852
Justin Plock <jplock@smoketurner.com>
2016-01-03 21:27:12 -0500

    2015 -> 2016

--- notification-api/src/test/java/com/smoketurner/notification/api/NotificationTest.java
+++ notification-api/src/test/java/com/smoketurner/notification/api/NotificationTest.java
@@ -1,23 +1,23 @@
 /**
- * Copyright 2015 Smoke Turner, LLC.
+ * Copyright 2016 Smoke Turner, LLC.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.smoketurner.notification.api;
 
 import static io.dropwizard.testing.FixtureHelpers.fixture;
 import static org.assertj.core.api.Assertions.assertThat;
-import io.dropwizard.jackson.Jackson;
 import java.util.TreeSet;
-import jersey.repackaged.com.google.common.collect.Sets;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.Before;
@@ -25,6 +25,8 @@ import org.junit.Test;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Sets;
+import io.dropwizard.jackson.Jackson;
 
 public class NotificationTest {
     private final ObjectMapper MAPPER = Jackson.newObjectMapper();
