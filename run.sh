bd7787cbd1d8848a3df35e8960472db2f7cae172
Justin Plock <jplock@smoketurner.com>
2017-02-18 10:45:43 -0500

    Clean up docker process

--- run.sh
+++ run.sh
@@ -2,4 +2,11 @@
 
 VERSION=`xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" pom.xml`
 
-docker run --rm=true -p 8080:8080 smoketurner/notification:${VERSION}
+docker run \
+--name notification \
+--rm \
+-e DW_DATACENTER_ID=1 \
+-e DW_WORKER_ID=1 \
+-e PORT=8080 \
+-p 8080:8080 \
+smoketurner/notification:${VERSION}
